285e00112f454cc1fd9283d4308c1281865835cc
William Robertson <c.william.robertson@gmail.com>
2023-09-13 10:40:14 +0200

    Tests for router

---
+++ server/__tests__/router.test.ts
@@ -0,0 +1,30 @@
+import { beforeAll, describe, expect, it } from 'bun:test'
+import request from 'supertest'
+import express from 'express'
+import router from 'router'
+
+describe('router', () => {
+  const app = express()
+
+  beforeAll(() => {
+    app.use('/', router)
+  })
+
+  describe('route "/"', async () => {
+    let response: request.Response
+
+    beforeAll(async () => {
+      response = await request(app).get('/')
+    })
+
+    it('responds with 200 OK', async () => {
+      expect(response.status).toEqual(200)
+    })
+
+    it('responds with JSON', async () => {
+      expect(response.headers['content-type']).toBe(
+        'application/json; charset=utf-8'
+      )
+    })
+  })
+})
