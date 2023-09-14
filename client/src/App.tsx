00e4f9bdf014f06a73d3d4b4a1df4f728a6c2de1
William Robertson <c.william.robertson@gmail.com>
2023-09-14 09:45:05 +0200

    Basic design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,50 @@
+import { Box, Grid, Paper, Typography } from '@mui/material'
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch'
+import { useEffect, useState } from 'react'
+
 function App() {
-  return <>this is the app</>
+  const [state, setState] = useState(null)
+
+  useEffect(() => {
+    if (state) return
+    fetch('/api')
+      .then((res) => res.json())
+      .then((res) => {
+        setState(res)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }, [state])
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+      }}
+    >
+      <Box sx={{ height: '90%', width: '90%' }}>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <RocketLaunchIcon fontSize="medium" />
+          <Typography variant="h5">Drivers</Typography>
+        </Box>
+        <Grid container spacing={4}>
+          <Grid item xs={12} md={6}>
+            <Paper>Something</Paper>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Paper>something elese</Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper>something more</Paper>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  )
 }
 
 export default App
