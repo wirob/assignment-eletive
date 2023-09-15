8b128afe64e1779c2d41f67cf43078f129b96938
William Robertson <c.william.robertson@gmail.com>
2023-09-15 09:40:34 +0200

    Move types to be shared across monorepo

--- server/drivers.ts
+++ server/drivers.ts
@@ -1,38 +1,3 @@
-type BenchMark = {
-  name: string
-  score: number
-}
-
-type Icon =
-  | 'balance'
-  | 'bubble'
-  | 'checkMark'
-  | 'computer'
-  | 'flower'
-  | 'heart'
-  | 'people'
-  | 'sentiment'
-  | 'trophy'
-
-type DriverName =
-  | 'Workload'
-  | 'Relationship with Colleagues'
-  | 'Meaningfulness and Participation'
-  | 'Workplace and Tools'
-  | 'Feedback and Commnucation'
-  | 'Health'
-  | 'Worklife balance'
-  | 'Happiness'
-  | 'Wellbeing'
-
-type Driver = {
-  name: DriverName
-  score: number
-  previousScore: number
-  icon: Icon
-  benchMark: BenchMark
-}
-
 const drivers: Driver[] = [
   {
     name: 'Workload',
